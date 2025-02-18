9de8c06d7501acee6dbd78756dccaf5cbfaa5fc8
PC <PC@DESKTOP-UGPI169>
2025-02-17 19:17:05 -0600

    actualizacion code

--- Laboratorio 6/Laboratorio 6.cpp
+++ Laboratorio 6/Laboratorio 6.cpp
@@ -20,14 +20,16 @@ public:
         cout << "El precio del carro es: " << precio << endl;
     }
 
-    // Métodos getter
+    virtual void obtenerInfo() { // Hacer la función virtual
+        cout << "\nMarca: " << marca << "\nModelo : " << modelo << "\nPrecio : $" << precio << endl;
+    }
+
     string getMarca() const {
         return marca;
     }
 
-    // Métodos setter
     void setMarca(const string& Marca) {
-        this->marca = Marca;  // Corregido: asignamos 'Marca' al atributo 'marca'
+        this->marca = Marca;  
     }
 
     string getModelo() const {
@@ -35,7 +37,7 @@ public:
     }
 
     void setModelo(const string& Modelo) {
-        this->modelo = Modelo;  // Corregido: asignamos 'Modelo' al atributo 'modelo'
+        this->modelo = Modelo;  
     }
 
     int getPrecio() const {
@@ -49,10 +51,72 @@ public:
 
 class Automovil : public Vehiculo {
 private:
-
+    int puertas;
+public:
+    Automovil(string _marca, string _modelo, int _precio, int _puertas) : Vehiculo(_marca,_modelo, _precio), puertas(_puertas) {
+        puertas = _puertas;
+    }
+    
+    void obtenerInfo() override { // Sobreescribe la función
+        Vehiculo::obtenerInfo();
+        cout << "Numero de puertas: " << puertas << endl;
+    }
 };
+
+class Motorcicleta : public Vehiculo {
+private:
+    int cilindrada;
+public:
+    Motorcicleta(string _marca, string _modelo, int _precio, int _cilindrada) : Vehiculo(_marca, _modelo, _precio), cilindrada(_cilindrada) {
+        cilindrada = _cilindrada;
+    }
+
+    void obtenerInfo() override { // Sobreescribe la función
+        Vehiculo::obtenerInfo();
+        cout << "La cilindrada es de: " << cilindrada << endl;
+    }
+};
+
+class Camioneta : public Vehiculo {
+private:
+    int capacidad;
+public: 
+    Camioneta(string _marca, string _modelo, int _precio, int _capacidad) : Vehiculo(_marca, _modelo, _precio), capacidad(_capacidad) {
+        capacidad = _capacidad;
+    }
+
+    void obtenerInfo() override { // Sobreescribe la función
+        Vehiculo::obtenerInfo();
+        cout << "La capacidad es de: " << capacidad << " KG" << endl;
+    }
+};
+
+class Cliente {
+private: 
+    int edad;
+    string nombre;
+public: 
+    Cliente(int _edad, string _nombre) {
+        edad = _edad;
+        nombre = _nombre;
+    }
+
+    void comprarVehiculo(Vehiculo* v) {  
+        cout << nombre << " ha comprado un:  " << endl;
+        v->obtenerInfo(); 
+        cout << endl;
+    }
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Camioneta ca1("Toyota", "Toyata rav", 120000, 2000);
+    Motorcicleta m1("Yamaha", "R1", 15000, 1000);
+    Automovil a1("Mazda", "Mazda 6xg", 100000, 4);
+    Cliente c1(24, "Juan Carlos");
+
+    c1.comprarVehiculo(&ca1);
+    c1.comprarVehiculo(&m1);
+    c1.comprarVehiculo(&a1);
 }
